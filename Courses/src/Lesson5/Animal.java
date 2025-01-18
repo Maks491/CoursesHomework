380558f850e1beced15a5c8499aad8f8892a9d10
Maksim Zelenko <maksimzel876@gmail.com>
2025-01-18 20:08:18 +0300

    Lesson5

---
+++ Courses/src/Lesson5/Animal.java
@@ -0,0 +1,27 @@
+package Lesson5;
+
+public class Animal {
+    String name;
+    static int countAnimal;
+
+    public Animal(String name ) {
+        this.name=name;
+        countAnimal++;
+    }
+
+    public void run(int distance) {
+        System.out.println("animal have run" + distance + "m");
+
+    }
+
+    public void swim(int distance) {
+        System.out.println("animal have swim" + distance + "m");
+    }
+
+    public void getCountAnimal() {
+        System.out.println("number of animals" + " " + " " + countAnimal);
+
+    }
+
+
+}
